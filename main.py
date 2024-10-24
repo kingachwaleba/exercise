772bdd87b3e989fab93b8f339938a053c1e6d81c
kingachwaleba <kingch6@gmail.com>
2024-10-24 18:34:22 +0200

    Upload a local file

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import os
+
 import boto3
-from boto3 import client
+from botocore.exceptions import ClientError
 
 BUCKET_NAME = "developer-task"
 PREFIX = "y-wing/"
@@ -14,3 +16,16 @@ def list_files():
     for file in response.get("Contents"):
         print(file["Key"])
 
+def upload_file(file_name, bucket, object_name=None):
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as error:
+        print(error)
+        return False
+    return True
+
+with open("FILE_NAME", "rb") as f:
+    s3_client.upload_fileobj(f, "amzn-s3-demo-bucket", "OBJECT_NAME")
